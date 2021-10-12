99a7680e0262b0dc2a782b3b3bbc9e12e8bcfd01
lockdef <locktki@icloud.com>
2021-10-13 00:04:34 +0900

    submit: ３週目

--- 3.py
+++ 3.py
@@ -1,27 +1,30 @@
 import numpy as np
+import math as m
 import matplotlib.pyplot as plt
 
 
 def sin(x):
     a = x
-    for i in range(1, 2):
-        a += ((-1)**i) * (x**(2 * i + 1)) / (2 * i + 1)
+    for i in range(2):
+        a += ((-1)**i) * (x**(2 * i + 1)) / m.factorial(2 * i + 1)
     b = x
-    for i in range(1, 4):
-        b += ((-1)**i) * (x**(2 * i + 1)) / (2 * i + 1)
+    for i in range(4):
+        b += ((-1)**i) * (x**(2 * i + 1)) / m.factorial(2 * i + 1)
     c = x
-    for i in range(1, 6):
-        c += ((-1)**i) * (x**(2 * i + 1)) / (2 * i + 1)
+    for i in range(6):
+        c += ((-1)**i) * (x**(2 * i + 1)) / m.factorial(2 * i + 1)
     d = np.sin(x)
     return abs(a - d), abs(b - d), abs(c - d)
 
 
-x = np.linspace(0.0, 3.14, 100)
+x = np.linspace(0.0, np.pi, 100)
 
 
 y1 = []
 y2 = []
 y3 = []
+
+print(x)
 
 for i in x:
     y1.append(sin(i)[0])
